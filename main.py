b13c1f75bc067012aed9d4cec8710016886ce92b
Ivan Zolotov <vanchpuck@mail.ru>
2020-08-18 22:45:23 +0300

    use model dump

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import NearestNeighbors
+from joblib import load
 import logging
 import boto3
-import itertools
 import numpy as np
 
 S3 = boto3.resource('s3')
 VOCABULARY_BUCKET = "org.gear-scanner.data"
 MODELS = dict()
-
-
-# def synonyms_preprocessor(string, synonyms):
-#     for i in synonyms:vm_modelpair
-#         string = string.replace(i[0], i[1])
-#     return string
 
 
 def lambda_handler(event, context):
@@ -24,26 +18,22 @@ def lambda_handler(event, context):
         if "brand" in message:
             brand = message["brand"]
             if brand not in MODELS:
-                MODELS[brand] = create_model(brand)
+                MODELS[brand] = read_model_dump(VOCABULARY_BUCKET, "product-classifier/model/"+brand.lower())
             original_name = classify(brand, message["name"])
             message["originalName"] = original_name
             print(message)
 
 
-def read_brand_products(brand):
-    obj = S3.Object(VOCABULARY_BUCKET, brand.lower() + "-vocabulary.txt")
-    return list(map(lambda line: line.decode('utf-8'), obj.get()['Body'].iter_lines()))
+def read_model(path):
+    model = S3.Object(VOCABULARY_BUCKET, path)
+    load(model)
 
 
-def create_model(brand):
-    brand_products = read_brand_products(brand)
-    vocabulary = [sub.split() for sub in brand_products]
-    vocabulary = set(itertools.chain.from_iterable(vocabulary))
-    vectorizer = CountVectorizer(ngram_range=(1, 2), binary=True, vocabulary=vocabulary)
-    term_document_matrix = vectorizer.fit_transform(brand_products)
-    nn_model = NearestNeighbors(n_neighbors=3)
-    nn_model.fit(term_document_matrix.toarray())
-    return brand_products, vectorizer, nn_model
+def read_model_dump(bucket, key):
+    local_path = "/tmp/" + key.replace("/", "-")
+    S3.Bucket(bucket).download_file(key, local_path)
+    with open(local_path, 'rb') as file:
+        return load(file)
 
 
 def classify(brand, product_name):
@@ -60,12 +50,13 @@ def classify(brand, product_name):
 
 
 # if __name__ == "__main__":
-    # # execute only if run as a script
-    # vm_pair = create_model("petzl")
-    # doc_term_matrix = vm_pair[1].transform(["lynx crampon"]).toarray()
-    # nearest = vm_pair[2].kneighbors(doc_term_matrix)
-    #
-    # if nearest[0][0][0] == nearest[0][0][2]:
-    #     print("unknown2")
-    # else:
-    #     print(vm_pair[0][nearest[1][0][0]])
+#     vm_pair = read_model_dump(VOCABULARY_BUCKET, "product-classifier/model/petzl")
+#     # execute only if run as a script
+#     # vm_pair = create_model("petzl")
+#     doc_term_matrix = vm_pair[1].transform(["lynx crampon"]).toarray()
+#     nearest = vm_pair[2].kneighbors(doc_term_matrix)
+#
+#     if nearest[0][0][0] == nearest[0][0][2]:
+#         print("unknown2")
+#     else:
+#         print(vm_pair[0][nearest[1][0][0]])
